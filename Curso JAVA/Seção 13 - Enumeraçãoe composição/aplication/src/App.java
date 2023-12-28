02495b7808c00a8506afdf542b52d47043543238
Gabriel Fischer <gabrielmullerfischer@gmail.com>
2023-12-28 09:03:48 -0300

    Seção 13 - composição

--- Curso JAVA/Seção 13 - Enumeraçãoe composição/aplication/src/App.java
+++ Curso JAVA/Seção 13 - Enumeraçãoe composição/aplication/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Order order = new Order(1080, new Date(), OrderStatus.pendingPayment);
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.delivered;
+
+        OrderStatus os2 = OrderStatus.valueOf("delivered");
+
+        System.out.println(os1);
+        System.out.println(os2);
+
     }
 }
---
+++ Curso JAVA/Seção 13 - Enumeraçãoe composição/aplication/src/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package entities.enums;
+
+public enum OrderStatus {
+
+    pendingPayment,
+    processing,
+    shipped,
+    delivered;
+}
