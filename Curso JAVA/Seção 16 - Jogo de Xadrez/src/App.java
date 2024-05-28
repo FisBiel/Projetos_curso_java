e14113fa398e569925a5595d5aca9ecf2ad84ea2
Gabriel Fischer <gabrielmullerfischer@gmail.com>
2024-05-28 17:24:16 -0300

    Posição xadrez

---
+++ Curso JAVA/Seção 16 - Jogo de Xadrez/src/App.java
@@ -0,0 +1,9 @@
+import tabuleiro.Posição;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Posição pos = new Posição(3, 5);
+        System.out.println(pos);
+    }
+}
